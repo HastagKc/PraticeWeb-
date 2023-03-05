1aa3c9e23c54cadec8ae5879b64f4ab1153c7275
HastagKc <chaudharykshittiz950@gmail.com>
2023-03-05 20:03:03 +0545

    adding auto referesh in js after submit

---
+++ Complete WebPratice/project4/script.js
@@ -0,0 +1,42 @@
+const questionE = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+// geting random number
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+// multipling two number
+const correctAns = num1 * num2;
+
+// show to answer section
+questionE.innerText = `What is ${num1} multiply by ${num2} ?`;
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+  score = 0;
+}
+
+scoreEl.innerText = `Score: ${score}`;
+
+formEl.addEventListener("submit", function (event) {
+  //prevent form from submitting and reloading page
+  event.preventDefault();
+  const userAns = +inputEl.value;
+
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage();
+    window.location.reload();
+  } else {
+    score--;
+    updateLocalStorage();
+    window.location.reload();
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
